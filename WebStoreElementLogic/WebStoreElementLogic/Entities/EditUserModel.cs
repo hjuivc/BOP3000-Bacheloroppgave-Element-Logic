463b0de6c89ae8af656912b26d08c8cda99cc15f
Bjørnar Borge <72221999+bjornarborge@users.noreply.github.com>
2023-04-07 15:42:18 +0200

    Create EditUserModel.cs

---
+++ WebStoreElementLogic/WebStoreElementLogic/Entities/EditUserModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebStoreElementLogic.Entities
+{
+    public class EditUserModel
+    {
+        public int userId { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+    }
+
+}

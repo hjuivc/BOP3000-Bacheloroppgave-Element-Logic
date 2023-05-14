3dc26c634d8b5c14c7963c10768b4c49c445d50a
Bjørnar Borge <72221999+bjornarborge@users.noreply.github.com>
2023-05-14 18:08:46 +0200

    Create IUserService.cs

---
+++ WebStoreElementLogic/WebStoreElementLogic/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using WebStoreElementLogic.Entities;
+namespace WebStoreElementLogic.Interfaces
+{
+    public interface IUserService
+    {
+        Task<int> Create(User user);
+        Task<List<User>> GetNextID(int userId);
+        Task<User> GetUser(string userName);
+        Task<List<User>> GetAllUsersAsync();
+        Task<User> GetUserById(int id);
+
+        Task<int> DeleteUserAsync(int id);
+        Task<int> UpdateUserAsync(EditUserModel editUser);
+
+    }
+}

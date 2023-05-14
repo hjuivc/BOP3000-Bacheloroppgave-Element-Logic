57dd66e1249419485ba6108b524a7f20691b5797
Bjørnar Borge <72221999+bjornarborge@users.noreply.github.com>
2023-05-14 18:12:37 +0200

    Restructured the program.cs file so be more method based.

--- WebStoreElementLogic/WebStoreElementLogic/Program.cs
+++ WebStoreElementLogic/WebStoreElementLogic/Program.cs
@@ -6,72 +6,154 @@ using WebStoreElementLogic.Hubs;
 using WebStoreElementLogic.Interfaces;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using System.Net;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WebStoreElementLogic.Account;
+using WebStoreElementLogic.Shared;
 using AppContext = WebStoreElementLogic.Data.AppContext;
 
-internal class Program
+namespace MyProgram
 {
-    private static void Main(string[] args)
+    public class Startup
     {
-        var builder = WebApplication.CreateBuilder(args);
+        public IConfiguration Configuration { get; }
 
-        // Add services to the container.
-        builder.Services.AddRazorPages();
-        builder.Services.AddServerSideBlazor();
-        builder.Services.AddSignalR();
-        builder.Services.AddControllers();
-
-        builder.Services.AddScoped<HttpClient>();
-        builder.Services.AddScoped<IEManagerService, EManagerService>();
-
-        builder.Services.AddTransient<IProductService, ProductService>();
-        builder.Services.AddTransient<DapperService>();
-
-        builder.Services.AddTransient<IInboundService, InboundService>();
-        builder.Services.AddTransient<IOrderService, OrderService>();
-
-        builder.Services.AddTransient<IDbConnection>(x => new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-        builder.Services.AddScoped<IDapperService, DapperService>();
-
-        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-        builder.Services.AddDbContext<AppContext>(options => options.UseSqlServer(connectionString));
-        builder.WebHost.ConfigureKestrel(options =>
+        public Startup(IConfiguration configuration)
         {
-            options.Listen(IPAddress.Any, 5000, listenOptions =>
-            {
-                listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
-            });
-            options.Listen(IPAddress.Any, 7001, listenOptions =>
-            {
-                listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
-                listenOptions.UseHttps();
-            });
-        });
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
+            Configuration = configuration;
         }
 
-        //app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseEndpoints(endpoints =>
+        public void ConfigureServices(IServiceCollection services)
         {
-            endpoints.MapControllers();
-            endpoints.MapHub<EManagerHub>("/EManagerHub");
-            endpoints.MapBlazorHub();
-            endpoints.MapFallbackToPage("/_Host");
-        });
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSignalR();
+            services.AddControllers();
 
-        app.Run();
+            // Add distributed memory cache
+            services.AddDistributedMemoryCache();
+
+            services.AddTransient<IProductService, ProductService>();
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IDapperService, DapperService>();
+            services.AddTransient<IInboundService, InboundService>();
+            services.AddTransient<IOrderService, OrderService>();
+
+            services.AddDbContext<WebStoreElementLogic.Data.AppContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<WebStoreElementLogic.Data.AppContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/login";
+                    options.LogoutPath = "/logout";
+                });
+
+            services.AddScoped<CustomAuthenticationStateProvider>();
+            services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<CustomAuthenticationStateProvider>());
+
+            // Add session middleware configuration
+            services.AddSession(options =>
+            {
+                options.Cookie.IsEssential = true;
+            });
+
+            // Add HttpClient service
+            services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://api.example.com") });
+            services.AddScoped<HttpClient>();
+            services.AddScoped<IEManagerService, EManagerService>();
+            services.AddScoped<IDapperService, DapperService>();
+
+
+        }
+
+
+
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseSession();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add the Startup class
+            builder.Services.AddTransient<Startup>();
+
+            // Add the ConfigureServices method from the Startup class
+            var startup = new Startup(builder.Configuration);
+            startup.ConfigureServices(builder.Services);
+
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.Listen(IPAddress.Any, 5000, listenOptions =>
+                {
+                    listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+                });
+                options.Listen(IPAddress.Any, 7001, listenOptions =>
+                {
+                    listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+                    listenOptions.UseHttps();
+                });
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            // Configure the request pipeline
+            startup.Configure(app, app.Environment);
+
+            app.Run();
+        }
     }
 }

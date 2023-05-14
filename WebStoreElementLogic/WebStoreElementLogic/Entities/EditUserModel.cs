b218ecd3572203e335a1e3aaeb61c6597cd1e6b5
Bjørnar Borge <72221999+bjornarborge@users.noreply.github.com>
2023-05-14 18:08:13 +0200

    A dedicated class for editing users.

---
+++ WebStoreElementLogic/WebStoreElementLogic/Entities/EditUserModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebStoreElementLogic.Entities
+{
+    public class EditUserModel
+    {
+        public int userId { get; set; }
+
+        [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "Username can only contain letters and numbers.")]
+        public string userName { get; set; }
+
+        public string password { get; set; }
+
+        public bool admin { get; set; }
+    }
+
+}

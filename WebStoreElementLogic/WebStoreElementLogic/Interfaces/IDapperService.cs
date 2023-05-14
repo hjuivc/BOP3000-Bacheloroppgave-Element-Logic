bceb8f82f9a2d434a3330ffe3e9689c541efd84a
Bjørnar Borge <72221999+bjornarborge@users.noreply.github.com>
2023-05-14 18:08:41 +0200

    Update IDapperService.cs

--- WebStoreElementLogic/WebStoreElementLogic/Interfaces/IDapperService.cs
+++ WebStoreElementLogic/WebStoreElementLogic/Interfaces/IDapperService.cs
@@ -9,6 +9,8 @@ namespace WebStoreElementLogic.Interfaces
         List<T> GetAll<T>(string sql, object value, CommandType commandType);
         T Insert<T>(string sql, DynamicParameters dbPara, CommandType commandType);
         int Execute(string sql, object parameters = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null);
+        Task<IEnumerable<TEntity>> GetAllAsync<TEntity>(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null) where TEntity : class;
+        Task<int> ExecuteAsync(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null);
 
         IDbConnection Connection { get; }
     }

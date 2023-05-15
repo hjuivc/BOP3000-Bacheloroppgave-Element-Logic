53eca8065e6c17db4c120162d6a90f72ce12968e
Eiriksm0raas <eirik.sm0raas@gmail.com>
2023-05-15 16:49:42 +0200

    Replaced PGBody with an entity class

--- WebStoreElementLogic/WebStoreElementLogic/Controllers/EManagerController.cs
+++ WebStoreElementLogic/WebStoreElementLogic/Controllers/EManagerController.cs
@@ -6,6 +6,7 @@ using System.Xml.Linq;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.StaticFiles;
 using WebStoreElementLogic.Interfaces;
+using WebStoreElementLogic.Entities;
 
 namespace WebStoreElementLogic.Controllers
 {
@@ -42,8 +43,8 @@ namespace WebStoreElementLogic.Controllers
 
             Console.WriteLine(xml);
 
-            
-            PGBody[] receipts = PGBody.FromXml(xml);
+
+            PlacedGoodsBody[] receipts = PlacedGoodsBody.FromXml(xml);
 
             // TODO: change to no loop
             // Update database with PG info
@@ -71,22 +72,6 @@ namespace WebStoreElementLogic.Controllers
             string xml = await reader.ReadToEndAsync();
 
             Console.WriteLine(xml);
-
-            return Ok();
-
-
-            PGBody[] receipts = PGBody.FromXml(xml);
-
-            // Update database with PG info
-            foreach (var receipt in receipts)
-            {
-                //await _inboundService.Update(receipt.PurchaseOrderId);
-                Console.WriteLine($"Got PG from EManager: {receipt.ExtProductId}");
-            }
-
-            // Alert connected clients TODO: replace 10 with actual data
-            await _hubContext.Clients.All.SendAsync("ConfirmedPicklist", receipts);
-
 
             return Ok();
         }
@@ -136,47 +121,6 @@ namespace WebStoreElementLogic.Controllers
                 }
 
                 return cpBodies.ToArray();
-            }
-        }
-
-        // Consider as data class?
-        public class PGBody
-        {
-            public int TransactionId { get; set; }
-            public string PurchaseOrderId { get; set; }
-            public int PurchaseOrderLineId { get; set; }
-            public int ExtProductId { get; set; }
-            public decimal Quantity { get; set; }
-
-            public override string ToString()
-            {
-                return $"PrdouctId: {ExtProductId}\nQuantity: {Quantity}";
-            }
-
-            // TODO: hvis transaction id ikke er int, så funker ikke parsen. Bruk try/catch
-            public static PGBody[] FromXml(string xml)
-            {
-                XDocument doc = XDocument.Parse(xml);
-                var transactionId = int.Parse(doc.Root.Element("TRANSACTIONID").Value);
-                var putaways = doc.Root.Elements("PUTAWAY");
-                List<PGBody> pgBodies = new List<PGBody>();
-
-                foreach (var putaway in putaways)
-                {
-                    pgBodies.Add(new PGBody
-                    {
-                        TransactionId = transactionId,
-                        PurchaseOrderId = putaway.Element("PURCHASEORDERID").Value,
-                        PurchaseOrderLineId = int.Parse(putaway.Element("PURCHASEORDERLINEID").Value),
-                        ExtProductId = int.Parse(putaway.Element("EXTPRODUCTID").Value),
-                        Quantity = decimal.Parse(
-                            putaway.Element("ACTQUANTITY").Value,
-                            System.Globalization.CultureInfo.InvariantCulture
-                        )
-                    });
-                }
-
-                return pgBodies.ToArray();
             }
         }
     }
---
+++ WebStoreElementLogic/WebStoreElementLogic/Entities/PlacedGoods.cs
@@ -0,0 +1,43 @@
+﻿using System.Xml.Linq;
+
+namespace WebStoreElementLogic.Entities
+{
+    public class PlacedGoodsBody
+    {
+        public int TransactionId { get; set; }
+        public string PurchaseOrderId { get; set; }
+        public int PurchaseOrderLineId { get; set; }
+        public int ExtProductId { get; set; }
+        public decimal Quantity { get; set; }
+
+        public override string ToString()
+        {
+            return $"PrdouctId: {ExtProductId}\nQuantity: {Quantity}";
+        }
+
+        public static PlacedGoodsBody[] FromXml(string xml)
+        {
+            XDocument doc = XDocument.Parse(xml);
+            var transactionId = int.Parse(doc.Root.Element("TRANSACTIONID").Value);
+            var putaways = doc.Root.Elements("PUTAWAY");
+            List<PlacedGoodsBody> pgBodies = new List<PlacedGoodsBody>();
+
+            foreach (var putaway in putaways)
+            {
+                pgBodies.Add(new PlacedGoodsBody
+                {
+                    TransactionId = transactionId,
+                    PurchaseOrderId = putaway.Element("PURCHASEORDERID").Value,
+                    PurchaseOrderLineId = int.Parse(putaway.Element("PURCHASEORDERLINEID").Value),
+                    ExtProductId = int.Parse(putaway.Element("EXTPRODUCTID").Value),
+                    Quantity = decimal.Parse(
+                        putaway.Element("ACTQUANTITY").Value,
+                        System.Globalization.CultureInfo.InvariantCulture
+                    )
+                });
+            }
+
+            return pgBodies.ToArray();
+        }
+    }
+}

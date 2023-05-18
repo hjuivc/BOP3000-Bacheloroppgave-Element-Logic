08981a6fb09868d9156ff30fb4de94277fd7720e
Evenmyk <73118778+Evenmyk@users.noreply.github.com>
2023-05-18 23:53:13 +0200

    små fixes til inbound

--- WebStoreElementLogic/WebStoreElementLogic/Entities/Inbound.cs
+++ WebStoreElementLogic/WebStoreElementLogic/Entities/Inbound.cs
@@ -8,19 +8,19 @@ namespace WebStoreElementLogic.Entities
         public int TransactionId { get; set; }
         public string PurchaseOrderId { get; set; }
         public int PurchaseOrderLineId { get; set; }
-        public string ExtProductId { get; set; }
+        public string ProductId { get; set; }
         public decimal Quantity { get; set; }
         public bool Status { get; set; }
         public string? Name { get; set; }
         public int DisplayQuantity => (int)Quantity;
 
-        public Inbound(int inboundId, int transactionId, string purchaseOrderId, int purchaseOrderLineId, string extproductId, decimal quantity, bool status) 
+        public Inbound(int inboundId, int transactionId, string purchaseOrderId, int purchaseOrderLineId, string productId, decimal quantity, bool status) 
         {
             InboundId = inboundId;
             TransactionId = transactionId;
             PurchaseOrderId = purchaseOrderId;
             PurchaseOrderLineId= purchaseOrderLineId;
-            ExtProductId = extproductId;
+            ProductId = productId;
             Quantity = quantity;
             Status = status;
 
